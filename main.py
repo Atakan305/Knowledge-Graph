cb2cf73b70e1eb43d2fec58e18dae39e68485215
Atakan Erdoğan <76012121+Atakan305@users.noreply.github.com>
2023-10-28 16:55:06 +0200

    main.py

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import re
 
-# Create a new DiGraph (Directed Graph)
+#Create a new DiGraph (Directed Graph)
 G = nx.DiGraph()
 
-# Define the directory where your code files are stored
+#Define the directory where your code files are stored
 code_directory = "your/path/code/directory"
 
-# Define the supported programming languages and their corresponding file extensions
+#Define the supported programming languages and their corresponding file extensions
 languages = {
     "python": [".py"],
     "matlab": [".m"],
 }
 
-# Function to extract relationships from code
+#Function to extract relationships from code
 def extract_relationships(code, language):
     relationships = []
     if language == "python":
-        # Extract object creation, variable assignments, class definitions, module imports, dataflow, database connections, and API calls
+        #Extract object creation, variable assignments, class definitions, module imports, dataflow, database connections, and API calls
         object_creations = re.findall(r'(\w+)\s*=\s*(\w+)\(.+\)', code)
         variable_assignments = re.findall(r'(\w+)\s*=\s*.+', code)
         class_definitions = re.findall(r'classdef\s+(\w+)', code)
@@ -27,7 +27,7 @@ def extract_relationships(code, language):
         dataflow = re.findall(r'(\w+)\s*=\s*(\w+)\(.+\)', code)  # Extract source and target from dataflow
         database_connections = re.findall(r'\w+\(.+\)', code)  # Assuming this pattern for database connections
         api_calls = re.findall(r'(\w+)\(.+\)', code)  # Extract only function names as API calls
-        # Add a custom "type" based on your criteria, for example, function names
+        #Add a custom "type" based on your criteria, for example, function names
         relationships.extend((source, target, "function") for source, target in object_creations)
         relationships.extend((source, target, "variable") for source in variable_assignments)
         relationships.extend((source, target, "class") for source, target in class_definitions)
@@ -36,13 +36,13 @@ def extract_relationships(code, language):
         relationships.extend((source, target, "database") for source in database_connections)
         relationships.extend((source, function_name, "api") for function_name in api_calls)
     elif language == "matlab":
-        # Extract relationships from MATLAB code (customize for your specific MATLAB code structure)
+        #Extract relationships from MATLAB code
         class_definitions = re.findall(r'classdef\s+(\w+)', code)
         function_modules = re.findall(r'function\s+(\w+)', code)
         relationships.extend((source, target, "class") for source in class_definitions for target in function_modules)
     return relationships
 
-# Parse code files in the specified directory
+#Parse code files in the specified directory
 for root, dirs, files in os.walk(code_directory):
     for file in files:
         file_path = os.path.join(root, file)
@@ -59,16 +59,16 @@ for root, dirs, files in os.walk(code_directory):
 
         relationships = extract_relationships(code, language)
 
-        # Add nodes and edges to the graph
+        #Add nodes and edges to the graph
         for source, target, rel_type in relationships:
             G.add_node(source)
             G.add_node(target)
             G.add_edge(source, target, type=rel_type)
 
-# Draw the graph using the spring layout algorithm
+#Draw the knowledge graph using the "spring layout" algorithm
 pos = nx.spring_layout(G, seed=42)
 
-# Define edge colors
+#Define edge colors
 edge_colors = {
     "uses": "blue",
     "calls": "red",
@@ -81,20 +81,20 @@ edge_colors = {
     "api": "black",
 }
 
-# Draw the graph with node colors
+#Draw the graph with node colors
 nodes = nx.draw_networkx_nodes(G, pos, node_color="lightgray")
 
-# Draw the edges with edge colors
+#Draw the edges with edge colors
 edges = nx.draw_networkx_edges(G, pos, edge_color=[edge_colors.get(data.get("type", "black")) for _, _, data in G.edges(data=True)])
 
-# Add labels to the nodes
+#Add labels to the nodes
 nx.draw_networkx_labels(G, pos, labels={node: node for node in G.nodes()})
 
-# Add labels to the edges
+#Add labels to the edges
 edge_labels = {(source, target): rel_type for source, target, rel_type in G.edges(data="type")}
 nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
 
-# Display the graph with the title and without axis
+#Display the graph with the title and without axis
 plt.title("Knowledge Graph")
 plt.axis("off")
 plt.show()
