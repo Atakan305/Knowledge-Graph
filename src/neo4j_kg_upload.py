8dd9e92691bc8be50c53757ac48836dbc134c8e5
Atakan Erdoğan <76012121+Atakan305@users.noreply.github.com>
2023-10-28 15:33:21 +0200

    Add files via upload

---
+++ src/neo4j_kg_upload.py
@@ -0,0 +1,50 @@
+from neo4j import GraphDatabase
+import os
+
+class KnowledgeGraphImporter:
+    def __init__(self, uri, user, password):
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+    
+    def close(self):
+        self._driver.close()
+
+    def import_knowledge_graph(self, tx, graph_file):
+        # Open the knowledge graph file and read its content
+        with open(graph_file, 'r') as f:
+            knowledge_graph_data = f.read()
+
+        lines = knowledge_graph_data.split('\n')
+
+        for line in lines:
+            parts = line.split(',')
+            if len(parts) == 3:
+                source_node, target_node, relationship_type = parts[0], parts[1], parts[2]
+
+                # Customize the import logic to match your knowledge graph data structure
+                # For example, create nodes and relationships in Neo4j based on the parsed data
+                # You may need to use Cypher queries for this task
+                # For example, create a query with using Cypher. 
+                cypher_query = (
+                    f"MERGE (source:Node {{name: $source_node}})"
+                    f"MERGE (target:Node {{name: $target_node}})"
+                    f"MERGE (source)-[:{relationship_type}]->(target)"
+                )
+
+                tx.run(cypher_query, source_node=source_node, target_node=target_node)
+    
+def main():
+    # Define the Neo4j connection details
+    neo4j_uri = "neo4j://localhost:7687"
+    neo4j_user = "neo4j"
+    neo4j_password = "your_password"
+
+    # Define the path to your knowledge graph file
+    knowledge_graph_file = "knowledge_graph.txt"
+
+    importer = KnowledgeGraphImporter(neo4j_uri, neo4j_user, neo4j_password)
+
+    with importer._driver.session() as session:
+        session.write_transaction(importer.import_knowledge_graph, knowledge_graph_file)
+
+if __name__ == "__main__":
+    main()
